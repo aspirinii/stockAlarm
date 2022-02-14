8edae435deaf1b82206f7b6302bc065ed36b1e51
aspirinii <aspiriniijk@gmail.com>
2022-02-14 16:29:18 +0900

    combine scheduelAndTray

--- systemtray.py
+++ systemtray.py
@@ -1,11 +1,32 @@
 from pystray import MenuItem as item
 import pystray
 from PIL import Image
+import schedule
+import time
+import datetime
 
 def action():
     pass
 
+def job():
+    print("I'm working...",datetime.datetime.now())
+
+
+def scheduleTest():
+    schedule.every(2).seconds.do(job)
+    
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
 image = Image.open("cat.png")
-menu = (item('Start', action), item('Stop', lambda : icon.stop()))
+menu = (item('Start', lambda : scheduleTest()), item('Test', lambda : print("test")), item('Stop', lambda : icon.stop()))
 icon = pystray.Icon("What", image, "Cat", menu)
 icon.run()
+
+
+
+
+
+
